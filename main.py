2c3509c4c55b41c3969262c4eeb1d61bcb3c18b4
roodhouse <rughjm@gmail.com>
2023-11-17 11:46:13 -0600

    added dotenv

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
 
 '''
 Make sure the required packages are installed: 
@@ -26,9 +29,8 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -53,7 +55,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -75,6 +77,8 @@ class BlogPost(db.Model):
     comments = relationship("Comment", back_populates="parent_post")
     from os import getenv
     from dotenv import load_dotenv
+
+    load_dotenv()
 
 
 # Create a User table for all your registered users
